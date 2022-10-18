403932eaa5ce18128ef8a694cdd9ee89d8e0855d
Luis Chang <luis.chang@outlook.com>
2022-10-18 07:09:17 -0500

    Agregar archivos de proyecto.

---
+++ Semana07UESANSalesMVC.WEB/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Semana07UESANSalesMVC.WEB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
