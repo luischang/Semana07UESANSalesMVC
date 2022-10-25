63f180de5985a9470a4f7b4f870d7730a9d4c89a
Luis Chang <luis.chang@outlook.com>
2022-10-25 08:50:25 -0500

    View data, view bag & view model

--- Semana07UESANSalesMVC.WEB/Areas/Marketing/Controllers/HomeController.cs
+++ Semana07UESANSalesMVC.WEB/Areas/Marketing/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Semana07UESANSalesMVC.WEB.Areas.Marketing.Models;
 
 namespace Semana07UESANSalesMVC.WEB.Areas.Marketing.Controllers
 {
@@ -8,6 +10,40 @@ namespace Semana07UESANSalesMVC.WEB.Areas.Marketing.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult ProductsWithViewData() 
+        {
+            var products = GetProducts();
+            ViewData["ProductList"] = products;
+            ViewData["MyName"] = "My name is Luis Chang";
+
+            return View();
+        }
+
+        public IActionResult ProductsWithViewBag()
+        {
+            var products = GetProducts();
+            ViewBag.ProductList = products;
+            ViewBag.MyName = "My name is Luis Chang";
+            return View();
+        }
+
+        public IActionResult ProductsWithViewModel()
+        {
+            var products = GetProducts();
+            return View(products);
+        }
+
+
+
+        private IEnumerable<Product> GetProducts() 
+        {
+            var folder = Path.Combine(Directory.GetCurrentDirectory(), "Areas\\Marketing\\Data\\products.json");
+            var json = System.IO.File.ReadAllText(folder);
+
+            var products = JsonConvert.DeserializeObject<IEnumerable<Product>>(json);
+            return products;
         }
     }
 }
